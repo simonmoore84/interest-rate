0441bf7c0fe24483b8c72e45465ac02abad1d773
simonmoore <simon.moore-cic-uk@uk.ibm.com>
2016-01-13 08:57:41 +0000

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -8,16 +8,17 @@ angular.module('interestApp', [])
     $scope.interestHidden = true;
     $scope.newHidden = true;
     $scope.totalHidden = true;
+    $scope.varHidden = true;
     $scope.errorHidden = true;
 
     $scope.calculateTotalBalance = function() {
 
-        if($scope.balance >0 && $scope.balance <=1000)
+        if($scope.balance >0 && $scope.balance <1000)
             return $scope.errorHidden = true,
             $scope.newHidden = false,
             $scope.balance/100 * 1 + $scope.balance;
 
-        else if ($scope.balance >1000 && $scope.balance < 5000)
+        else if ($scope.balance >= 1000 && $scope.balance < 5000)
             return $scope.errorHidden = true,
             $scope.newHidden = false,
             $scope.balance/100 * 2 + $scope.balance;
@@ -36,28 +37,35 @@ angular.module('interestApp', [])
             return $scope.errorHidden = true,
             $scope.newHidden = true;
 
-        if ($scope.balance <=0) return
     }
 
     // Update with new variable interest rate
 
-    $scope.update = function() {
-        $scope.calculateTotalBalance = function() {
+    $scope.calculateVariableTotalBalance = function() {
+
+        if ($scope.interest > 0 )
             return $scope.balance/100 * $scope.interest + $scope.balance;
-        }
+
+        else if ($scope.interest <= 0)
+            return $scope.errorHidden = false,
+            $scope.varHidden = true;
+
+        else
+            $scope.varHidden = true;
+
     }
 
     // Reveal varibale interest rate form
 
     $scope.moreInterest = function() {
         $scope.interestHidden = false;
-        $scope.newHidden = true;
     }
 
     // Show total once values have been entered
 
     $scope.filterValue = function($event){
         $scope.totalHidden = false;
+        $scope.varHidden = false;
     };
 
 }])
--- js/test.js
+++ js/test.js
@@ -9,15 +9,62 @@ describe("Balance", function() {
         // The injector unwraps the underscores (_) from around the parameter names when matching
         $controller = _$controller_;
       }));
-
-      describe('$scope.balance', function() {
-        it('Test balance', function() {
+    
+    describe('$scope.balance', function() {
+        it('Test investment balance between 0 and 1000 produces the correct value with the set interest rate of 1%', function() {
           var $scope = {};
+            
+          $scope.balance = '500';
 
-          $scope.balance = '1000';
-          $scope.interest = '50';
+          $scope.balance/100 * 1 + $scope.balance;
+        });
+
+    });
+    
+    describe('$scope.balance', function() {
+        it('Test investment balance of alues that are equal to 1000 and above but less than 5000. This is to make sute it produces the correct value with the set interest rate of 2%', function() {
+          var $scope = {};
+            
+          $scope.balance = '2500';
+
+          $scope.balance/100 * 2 + $scope.balance;
+        });
+
+    });
+    
+    describe('$scope.balance', function() {
+        it('Test investment balance of over 5000', function() {
+          var $scope = {};
+          
+          $scope.balance = '5000';
+
+          return $scope.balance/100 * 3 + $scope.balance;
+        });
+
+    });
+    
+    describe('$scope.balance', function() {
+        it('Test investment values that are equal to and less than 0 (negative numbers).', function() {
+          var $scope = {};
+          
+          $scope.balance = '0';
+
+          return $scope.errorHidden = false, $scope.newHidden = true, $scope.totalHidden = true;
+
+        });
+
+    });
+    
+    describe('$scope.balance', function() {
+        it('Test investment value with variable rate interest value.', function() {
+          var $scope = {};
+          
+          $scope.balance = '2000';
+          $scope.interest = '25';
 
           return $scope.balance/100 * $scope.interest + $scope.balance;
+
+
         });
 
     });
