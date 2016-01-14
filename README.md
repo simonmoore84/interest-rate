12df893660451d8248709ebf21f65fcf88e9b203
simonmoore <simon.moore-cic-uk@uk.ibm.com>
2016-01-14 09:10:11 +0000

    Update Readme and some style and script updates

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Variable Interest Rate Application
 
+Clone the repositry from GIT to your local folder.
 
-After download run the html file to view the Application.
+After this has been cloned run the html file to view the Application.
 
+Install Node JS.
+
+Install NPM to install the relevant packages for the Jasmine and Karma test.
 
 To run the Jasmine tests with Karma run the following code within the command line from the project root directory;
 
--- js/script.js
+++ js/script.js
@@ -7,84 +7,69 @@ angular.module('interestApp', [])
 
     $scope.interestHidden = true;
     $scope.newHidden = true;
-    $scope.totalHidden = true;
-    $scope.varHidden = true;
-    $scope.errorHidden = true;
+    $scope.totalHidden = false;
+    $scope.varHidden = false;
 
     $scope.calculateTotalBalance = function() {
 
         if($scope.balance >0 && $scope.balance <1000)
-            return $scope.errorHidden = true,
-            $scope.newHidden = false,
+            return $scope.newHidden = false,
             $scope.balance/100 * 1 + $scope.balance;
 
         else if ($scope.balance >= 1000 && $scope.balance < 5000)
-            return $scope.errorHidden = true,
-            $scope.newHidden = false,
+            return $scope.newHidden = false,
             $scope.balance/100 * 2 + $scope.balance;
 
         else if ($scope.balance >= 5000)
-            return $scope.errorHidden = true,
-            $scope.newHidden = false,
+            return $scope.newHidden = false,
             $scope.balance/100 * 3 + $scope.balance;
 
+        else if (angular.isUndefined($scope.allowSomething))
+            $scope.totalHidden = true;
+        
         else if ($scope.balance == 0 )
-            return $scope.errorHidden = false,
-            $scope.newHidden = true,
-            $scope.totalHidden = true;
-        
-        else if ($scope.balance < 0 )
-            return $scope.errorHidden = false,
-            $scope.newHidden = true,
-            $scope.totalHidden = true;
-        
-        else if ($scope.balance == null )
-            $scope.totalHidden = true;
-
+            return $scope.newHidden = true,
+            $scope.totalHidden = false;
+            
         else
-            return $scope.errorHidden = true,
-            $scope.newHidden = true;
-            $scope.totalHidden = true;
-
+            return $scope.newHidden = true;
+            $scope.totalHidden = false;
     }
 
+    
     // Update with new variable interest rate
 
     $scope.calculateVariableTotalBalance = function() {
 
         if ($scope.interest > 0 )
             return $scope.balance/100 * $scope.interest + $scope.balance;
-
-        else if ($scope.interest <= 0)
-            return $scope.errorHidden = false,
-            $scope.varHidden = true;
+        
+        else if (angular.isUndefined($scope.allowSomething))
+            $scope.varHidden = false;
+        
+        else if ($scope.interest == 0 )
+            return $scope.varHidden = true;
 
         else
-            $scope.varHidden = true;
-
+            return $scope.varHidden = false;
     }
-
-    // Reveal varibale interest rate form
+    
+ 
+    // Reveal variable interest rate form
 
     $scope.moreInterest = function() {
         $scope.interestHidden = false;
     }
-
-    // Show total once values have been entered
-
+    
+    // Prevent Negative and string based characters being entered into the number field (Safari issue)
+    
     $scope.filterValue = function($event){
-        if ($scope.balance == '')
-            return $scope.totalHidden = true;
-        else {
-            $scope.totalHidden = false;
-        }
-            
-        $scope.varHidden = false;
         
         if(isNaN(String.fromCharCode($event.keyCode))){
             $event.preventDefault();
             
-            $scope.totalHidden = true;
+            $scope.totalHidden = false;
+            $scope.varHidden = false;
         }
     };
 
